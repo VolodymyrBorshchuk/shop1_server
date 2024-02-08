a8032d29e2b6ad8509c8faed26688abcab22911d
VolodymyrBorshchuk <vovkavovka000@gmail.com>
2024-02-08 13:10:35 +0200

    user authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeConfigService } from './config/sequelizeConfig.service';
 import { databaseConfig } from './config/configuration';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -16,6 +17,7 @@ import { databaseConfig } from './config/configuration';
       load: [databaseConfig],
     }),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule { }
